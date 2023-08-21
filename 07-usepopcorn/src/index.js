f4e6ea2168790b4e13d82088c49f8a24ea9a1474
shruti <shruti.licensed@gmail.com>
2023-08-21 10:14:06 +0530

    finished working on usePopcorn

--- 07-usepopcorn/src/index.js
+++ 07-usepopcorn/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { useState } from "react";
+// import { useState } from "react";
 import "./index.css";
 import App from "./App";
 // import StarRating from "./StarRating";
